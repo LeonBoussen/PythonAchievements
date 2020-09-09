9f6e1f0b14343ce69bf2d9436f03341bf0958daf
Leon Boussen <leon.boussen@hotmail.com>
2020-09-09 12:48:55 +0200

    Player Statistics Opdracht Python

---
+++ PlayerStatistics.py
@@ -0,0 +1,27 @@
+import time
+import sys
+
+
+name = 'Erwin Henraat'
+jumpforce = 20 #int
+specialpower = 'eleport'
+speed = 280.50 #float
+bike = 'Honda VFR VTEC'
+livesleft =  1j #complex
+ammo = 121 #int
+numberoflasers =  5 #int
+batterypower = 90.5 #floar
+level = 9000j #complex
+wanted = true
+
+print("Name = ",name)
+print("Jumpforce = ",name)
+print("Special Power = ", specialpower)
+print("Speed = ",speed)
+print("bike = ",bike)
+print("Lives Left =  ",livesleft)
+print("Ammo = ",ammo)
+print("numberoflasers",numberoflasers)
+print("batterypower",batterypower)
+print("level",level)
+print("Wanted = ", wanted)
